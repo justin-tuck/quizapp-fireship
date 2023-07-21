6282324e23cd95c8ef99eff6facb02fa95e7c286
justin-tuck <p.justin.tuck@gmail.com>
2023-07-21 06:39:44 -0400

    first commit

---
+++ android/app/src/main/kotlin/io/pjt/quizapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.pjt.quizapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/about/about.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
