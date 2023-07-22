becd327e45ea1170f5bb217a9dd80ed5039b1011
justin-tuck <p.justin.tuck@gmail.com>
2023-07-22 15:14:16 -0400

    small fix in auth and added json serializable for data classes

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:crypto/crypto.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AuthService {
