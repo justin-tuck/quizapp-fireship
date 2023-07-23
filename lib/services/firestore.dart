7fb73a70bb7ca78c023644e4715ab3c1cbc1e7cc
justin-tuck <p.justin.tuck@gmail.com>
2023-07-23 07:12:09 -0400

    Built out Firestore service for db calls

---
+++ lib/services/firestore.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  /// Reads all documents from the topics collection
+  Future<List<Topic>> getTopics() async {
+    var ref = _db.collection('topics');
+    var snapshot = await ref.get(); // one time listen
+    var data = snapshot.docs.map((s) => s.data());
+    var topics = data.map((d) => Topic.fromJson(d));
+    return topics.toList();
+  }
+
+  /// Retrieve one single quiz
+  Future<Quiz> getQuiz(String quizId) async {
+    var ref = _db.collection('quizzes').doc(quizId);
+    var snapshot = await ref.get();
+    return Quiz.fromJson(snapshot.data() ?? {});
+  }
+
+  /// Listens to current user's report document in Firestore
+  Stream<Report> streamReport() {
+    // switchMap is apart of rxdart
+    return AuthService().userStream.switchMap((user) {
+      if (user != null) {
+        var ref = _db.collection('reports').doc(user.uid);
+        return ref.snapshots().map((doc) => Report.fromJson(doc.data()!));
+      } else {
+        // Default report value
+        // Able to listen to report document in real time and update automatically
+        return Stream.fromIterable([Report()]);
+      }
+    });
+  }
+
+  /// Updates the current user's report document after completing a quiz
+  Future<void> updateUserReport(Quiz quiz) {
+    var user = AuthService().user!;
+    var ref = _db.collection('reports').doc(user.uid);
+
+    var data = {
+      'total': FieldValue.increment(1),
+      'topics': {
+        quiz.topic: FieldValue.arrayUnion([quiz.id])
+      }
+    };
+    // adding in set options merge true means it is no destructive and wont overwrite any existing data
+    return ref.set(data, SetOptions(merge: true));
+  }
+}
